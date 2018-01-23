ea0e907abc75fde23b2038ecfff3d373a8a4fd6c
D569906 <B4b1l0n14!>
2018-01-23 09:46:16 -0300

    joining djang and nginx deployment

--- cleanup.sh
+++ cleanup.sh
@@ -2,12 +2,13 @@
 
 
 kubectl delete -f mysql-deployment.yaml
-kubectl delete -f django-deployment.yaml
-kubectl delete -f nginx-rc.yaml
+#kubectl delete -f django-deployment.yaml
+#kubectl delete -f nginx-rc.yaml
+kubectl create -f nginx-django-deployment.yaml
 kubectl delete -f pv-volume.yaml
 kubectl delete configmap nginxconfigmap 
 kubectl delete secret mysql-pass
-cd ..
-cd heapster
-kubectl delete -f deploy/kube-config/influxdb/
-kubectl delete -f deploy/kube-config/rbac/heapster-rbac.yaml
+#cd ..
+#cd heapster
+#kubectl delete -f deploy/kube-config/influxdb/
+#kubectl delete -f deploy/kube-config/rbac/heapster-rbac.yaml
