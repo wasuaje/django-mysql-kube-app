6768f98da5346e5a6bd845eb636298582a032d7f
Wuelfhis Asuaje <wuelfhis.asuaje@jpmorgan.com>
2018-04-13 10:15:47 -0300

    debuging

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Contact, Signup
+
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+class SignupAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Signup, SignupAdmin)
